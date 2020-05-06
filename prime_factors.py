03899f2bd587b58a6d3a9ce83d4e080cd8c564cf
parisa-a <parisa.acharya22@ycdsbk12.ca>
2020-05-06 15:13:41 -0400

    code to prime factor input was added

---
+++ prime_factors.py
@@ -0,0 +1,21 @@
+import math
+
+# getting user input for integer 
+integer = int(input("Enter an integer (2 or greater): "))
+
+# determining if it is in the correct range (=>2) - need to find a way to break it 
+if integer < 2:
+    print("Incorrect value. Try again.")
+    exit()
+else: 
+    number = integer
+    print("The prime factors of" + " " + str(number) + " " + "are:" )
+
+# the process of factorization 
+factor = 2
+while factor <= number: 
+    if math.remainder(number, factor) == 0:
+        print(factor)
+        number = number // factor 
+    else: 
+        factor = factor + 1
